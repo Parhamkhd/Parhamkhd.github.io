25f5b06e71236b1c5ce051849e9e0435196497b8
Parhamkhd <31856253+Parhamkhd@users.noreply.github.com>
2019-02-09 20:42:06 +0330

    fixed ham icon in all of the HTML files

---
+++ assets/scripts/hamicon.js
@@ -0,0 +1,13 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    let y = document.getElementsByClassName('main-menu-items')[0].style.display;
+    if(y == ''){
+    document.getElementsByClassName('main-menu-items')[0].style.display = 'block';
+    }
+    else if(y == 'block'){
+      document.getElementsByClassName('main-menu-items')[0].style.display = 'none';
+    }
+    else if(y== 'none'){
+      document.getElementsByClassName('main-menu-items')[0].style.display = 'block';
+    }
+  }
